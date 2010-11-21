6e11ccdc3a0c24f51bdbb6bd8c66b19e36afb680
Ben Welsh <ben.welsh@gmail.com>
2010-11-21 12:44:33 -0800

    Got the openlayers example basically working without any selections

--- us_counties/urls.py
+++ us_counties/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('us_counties.views',
-    url(r'^openlayers/$', 'openlayers', name='us_counties-openlayers'),
-    url(r'^polymaps/$', 'polymaps', name='us_counties-polymaps'),
-    url(r'^raphael/$', 'raphael', name='us_counties-raphael'),
+    url(r'^openlayers/$', 'state_detail', {'template': 'openlayers.html'},
+        name='us_counties-openlayers'),
+    #url(r'^polymaps/$', 'polymaps', name='us_counties-polymaps'),
+    #url(r'^raphael/$', 'raphael', name='us_counties-raphael'),
 )
 
--- us_counties/views.py
+++ us_counties/views.py
@@ -1,20 +1,28 @@
-from django.http import HttpResponse
-from us_counties.models import County
+# Response helpers
+from django.http import Http404
 from django.views.generic.simple import direct_to_template
 
+# Models
+from us_counties.models import County
 
-def openlayers(request):
-    state = request.GET.get('state', 'CA')
+# State abbrevations and such
+from django.contrib.localflavor.us.us_states import STATE_CHOICES
+STATE_DICT = {}
+for x, y in STATE_CHOICES: STATE_DICT[x] = y
+
+
+def state_detail(request, template='openlayers.html'):
+    """
+    A simple example of how you could make a map of all the counties in a state.
+    """
+    state = request.GET.get('state', 'CA') or 'CA'
+    state_name = STATE_DICT.get(state, None)
+    if not state_name:
+        raise Http404
+    state_list = [dict(slug=i[0], name=i[1]) for i in STATE_CHOICES]
     object_list = County.objects.filter(state=state).only(
         'full_name', 'slug', 'simple_polygon_900913'
         )
-    template = 'openlayers.html'
     return direct_to_template(request, template, locals())
 
 
-def polymaps(request):
-    return HttpResponse("OK")
-
-
-def raphael(request):
-    return HttpResponse("OK")
