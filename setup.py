192734aa744fdfa303b088598b7807793c18854c
Ben Welsh <ben.welsh@gmail.com>
2010-11-21 17:39:28 -0800

    Tried Django style setup.py tricks

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 from setuptools import setup, find_packages
 
+data_files = []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+app_dir = 'us_counties'
+
+for dirpath, dirnames, filenames in os.walk(app_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    # Ignore any packages
+    if '__init__.py' in filenames:
+        pass
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
 setup(name='latimes-pluggablemaps-uscounties',
-      version='alpha-0.14',
+      version='alpha-0.16',
       description='L.A. Times Pluggable Maps: U.S. Counties',
       author='Ben Welsh',
       author_email='ben.welsh@gmail.com',
       url='http://github.com/datadesk/latimes-pluggablemaps-uscounties',
       download_url='http://github.com/datadesk/latimes-pluggablemaps-uscounties.git',
       packages=find_packages(),
+      data_files=data_files,
       license='MIT',
       keywords='gis geographical maps earth usa counties boundaries',
       classifiers=[
