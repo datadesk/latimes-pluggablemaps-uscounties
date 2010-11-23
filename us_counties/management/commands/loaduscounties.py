009508de28b0cf1883b211a9e7b43c769d95839a
palewire <ben.welsh@gmail.com>
2010-11-22 21:51:44 -0800

    Switched out trunk stdout writer from command and reverted to good ol print statements

--- us_counties/management/commands/loaduscounties.py
+++ us_counties/management/commands/loaduscounties.py
@@ -5,6 +5,6 @@ class Command(BaseCommand):
     help = 'Loads data for pluggable maps of U.S. counties'
 
     def handle(self, *args, **options):
-        self.stdout.write('Loading data for U.S. counties\n')
+        print 'Loading data for U.S. counties'
         load.all()
-        self.stdout.write('Successfully loaded data for U.S. counties\n')
+        print 'Successfully loaded data for U.S. counties'
