7a5c817a5e7b9393978f4feaae3cd20a2ce3528e
palewire <ben.welsh@gmail.com>
2010-11-23 06:50:43 -0800

    got the mouseover effects working with polymaps

--- us_counties/templates/us_counties/polymaps.js
+++ us_counties/templates/us_counties/polymaps.js
@@ -7,9 +7,12 @@ window.onload = function() {
         .zoom(4)
         .add(po.interact());
     map.add(po.image().url("http://s3.amazonaws.com/com.modestmaps.bluemarble/{Z}-r{Y}-c{X}.jpg"));
-    data = po.geoJson().url("/us-counties/json/?state={{ state }}").tile(false).on("load", load);
+    data = po.geoJson()
+        .url("/us-counties/json/?state={{ state }}")
+        .tile(false)
+        .on("load", load);
     map.add(data);
-    //map.add(po.compass().pan("none"));
+    map.add(po.compass().pan("none"));
 }
 
 Array.max = function( array ){
@@ -19,13 +22,16 @@ Array.min = function( array ){
     return Math.min.apply( Math, array );
 };
 
-
 function load(e) {
   var x = Array();
   var y = Array();
   for (var i = 0; i < e.features.length; i++) {
     var feature = e.features[i];
+    feature.element.setAttribute("id", i);
     feature.element.setAttribute("class", "county");
+    feature.element.setAttribute("name", feature.data.properties.name);
+    //feature.element.on("mouseover", function(){console.log(i);});
+    //feature.element.setAttribute
     var coords = feature.data.geometry.coordinates;
     var polygons = $.map(coords, function(a){return a});
     var points = $.map(polygons, function(a){return a});
@@ -39,5 +45,16 @@ function load(e) {
   yMax = Array.max(y);
   yMin = Array.min(y);
   map.extent([{lon:xMin,lat:yMin},{lon:xMax,lat:yMax}])
+    $(".county").hover(
+      function () {
+        $('#selected-object').html($(this).attr('name'));
+        var o = $('#' + $(this).attr("id"));
+        $(this).css("fill", "#DDD");
+      },
+      function () {
+        $('#selected-object').html('');
+        $(this).css("fill", "#007AAA");
+      }
+    );
 }
 
