22f737b587402b0e03879ffad482d18957fc6bfd
palewire <ben.welsh@gmail.com>
2010-11-23 07:04:52 -0800

    Kill cruft and added some inline docs

--- us_counties/templates/us_counties/polymaps.js
+++ us_counties/templates/us_counties/polymaps.js
@@ -1,17 +1,21 @@
 var data, map, yMax, xMax, yMin, xMin;
 window.onload = function() {
+    // Create the map
     var po = org.polymaps;
     map = po.map()
         .container(document.getElementById("map-canvas").appendChild(po.svg("svg")))
         .zoomRange([1, 7])
         .zoom(4)
         .add(po.interact());
+    // Add the tile layer
     map.add(po.image().url("http://s3.amazonaws.com/com.modestmaps.bluemarble/{Z}-r{Y}-c{X}.jpg"));
+    // Add the data layer
     data = po.geoJson()
         .url("/us-counties/json/?state={{ state }}")
         .tile(false)
         .on("load", load);
     map.add(data);
+    // Add some extras
     map.add(po.compass().pan("none"));
 }
 
@@ -26,12 +30,13 @@ function load(e) {
   var x = Array();
   var y = Array();
   for (var i = 0; i < e.features.length; i++) {
+    // Set all the data we need into the element
     var feature = e.features[i];
     feature.element.setAttribute("id", i);
     feature.element.setAttribute("class", "county");
     feature.element.setAttribute("name", feature.data.properties.name);
-    //feature.element.on("mouseover", function(){console.log(i);});
-    //feature.element.setAttribute
+    // Flatten the coordinates and add them to our master list for extent
+    // calculation
     var coords = feature.data.geometry.coordinates;
     var polygons = $.map(coords, function(a){return a});
     var points = $.map(polygons, function(a){return a});
@@ -40,15 +45,18 @@ function load(e) {
         if(!isNaN(a[1])){y.push(a[1])};
     });
   }
+  // Set the extent
   xMax = Array.max(x);
   xMin = Array.min(x);
   yMax = Array.max(y);
   yMin = Array.min(y);
   map.extent([{lon:xMin,lat:yMin},{lon:xMax,lat:yMax}])
+  // Set the event handlers
     $(".county").hover(
       function () {
+        // Set the headline
         $('#selected-object').html($(this).attr('name'));
-        var o = $('#' + $(this).attr("id"));
+        // Change the fill
         $(this).css("fill", "#DDD");
       },
       function () {
