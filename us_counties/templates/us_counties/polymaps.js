f76949f6dd20c1f1bc1152f7715fe3a1c94529f7
palewire <ben.welsh@gmail.com>
2010-11-22 23:56:44 -0800

    Forced polymaps to dynamically set the extent based on the JSON layer. Phew.

--- us_counties/templates/us_counties/polymaps.js
+++ us_counties/templates/us_counties/polymaps.js
@@ -1,4 +1,4 @@
-var data, map;
+var data, map, yMax, xMax, yMin, xMin;
 window.onload = function() {
     var po = org.polymaps;
     map = po.map()
@@ -7,9 +7,37 @@ window.onload = function() {
         .zoom(4)
         .add(po.interact());
     map.add(po.image().url("http://s3.amazonaws.com/com.modestmaps.bluemarble/{Z}-r{Y}-c{X}.jpg"));
-    data = po.geoJson().url("/us-counties/json/?state={{ state }}").tile(false);
+    data = po.geoJson().url("/us-counties/json/?state={{ state }}").tile(false).on("load", load);
     map.add(data);
-    map.add(po.compass().pan("none"));
+    //map.add(po.compass().pan("none"));
 }
 
+Array.max = function( array ){
+    return Math.max.apply( Math, array );
+};
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
+};
+
+
+function load(e) {
+  var x = Array();
+  var y = Array();
+  for (var i = 0; i < e.features.length; i++) {
+    var feature = e.features[i];
+    feature.element.setAttribute("class", "county");
+    var coords = feature.data.geometry.coordinates;
+    var polygons = $.map(coords, function(a){return a});
+    var points = $.map(polygons, function(a){return a});
+    $.map(points, function(a){
+        if(!isNaN(a[0])){x.push(a[0])}; 
+        if(!isNaN(a[1])){y.push(a[1])};
+    });
+  }
+  xMax = Array.max(x);
+  xMin = Array.min(x);
+  yMax = Array.max(y);
+  yMin = Array.min(y);
+  map.extent([{lon:xMin,lat:yMin},{lon:xMax,lat:yMax}])
+}
 
--- us_counties/urls.py
+++ us_counties/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('us_counties.views',
     url(r'^openlayers/$', 'state_detail', {'template': 'us_counties/openlayers.html'},
         name='us_counties_openlayers'),
-    #url(r'^polymaps/$', 'state_detail', {'template': 'polymaps.html'},
-    #    name='us_counties_polymaps'),
-    #url(r'^json/$', 'json', name='us_counties_json'),
+    url(r'^polymaps/$', 'state_detail', {'template': 'us_counties/polymaps.html'},
+        name='us_counties_polymaps'),
+    url(r'^json/$', 'json', name='us_counties_json'),
     #url(r'^raphael/$', 'raphael', name='us_counties-raphael'),
 )
 
